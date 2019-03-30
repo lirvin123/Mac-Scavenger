6cd59aa04abcf96160bc1848bd236122247f7e29
Lily <lily.irvin@gmail.com>
2019-03-30 14:43:04 -0500

    Hint penalty screens added

--- Screens/Main.js
+++ Screens/Main.js
@@ -1,52 +1,75 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image, Platform, Button } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Platform, Button, ScrollView, Alert } from 'react-native';
 import AppNavigator from '../navigator/appNavigator';
-import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import Swiper from 'react-native-swiper';
+import {BlurView} from 'expo';
 
 export default class Main extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      hintNum: 1
+      photo: 'box',
+      hintOneBlur: 100,
+      hintTwoBlur: 100,
+      hintOneUnlocked: false,
+      hintTwoUnlocked: false
     }
   }
 
-  onSwipeLeft(gestureState) {
-    if (this.state.hintNum == 3) { //Should change to be max eventually
-      return
+  onSwipe(index) {
+    if (index == 1 || index == 2) {
+      if ((index == 1 && this.state.hintOneUnlocked == true) || (index == 2 && this.state.hintTwoUnlocked == true)) {
+        return
+      }
+        if (index == 1) {
+          Alert.alert(
+            'Unlock',
+            '+5 Minute Penalty',
+            [
+              {text: 'Unlock', onPress: () => this.setState({ hintOneBlur: 0, hintOneUnlocked: true })},
+              {text: 'Go Back'}
+            ],
+            { cancelable: false }
+          )
+        }
+        else {
+          Alert.alert(
+            'Unlock',
+            '+7 Minute Penalty',
+            [
+              {text: 'Unlock', onPress: () => this.setState({ hintTwoBlur: 0, hintTwoUnlocked: true })},
+              {text: 'Go Back'}
+            ],
+            { cancelable: false }
+          )
+        }
+      }
     }
-    else {
-      this.setState({
-        hintNum: this.state.hintNum + 1
-      })
-    }
-  }
-
-  onSwipeRight(gestureState) {
-    if (this.state.hintNum == 1) {
-      return
-    }
-    else {
-      this.setState({
-        hintNum: this.state.hintNum - 1
-      })
-    }
-  }
 
   render() {
 
     return (
       <View style={styles.container}>
-        <GestureRecognizer
-          detectSwipeUp={false}
-          detectSwipeDown={false}
-          onSwipeLeft={(state) => this.onSwipeLeft(state)}
-          onSwipeRight={(state) => this.onSwipeRight(state)}>
-          <Image
-            source={{ uri: 'https://res.cloudinary.com/lirvin/image/upload/v1553715269/box' + this.state.hintNum + '.heic' }}
-            style={{ width: 300, height: 500}} />
-        </GestureRecognizer>
+        <Swiper loop={false} onIndexChanged={(index) => this.onSwipe(index)}>
+          <View style={styles.container}>
+            <Image
+              source={{ uri: 'https://res.cloudinary.com/lirvin/image/upload/v1553715269/' + this.state.photo + 1 + '.heic' }}
+              style={{ width: 325, height: 415}} />
+          </View>
+          <View style={styles.container}>
+            <Image
+              source={{ uri: 'https://res.cloudinary.com/lirvin/image/upload/v1553715269/' + this.state.photo + 2 + '.heic' }}
+              style={{ width: 325, height: 415}}
+              blurRadius={this.state.hintOneBlur} />
+          </View>
+          <View style={styles.container}>
+            <Image
+              source={{ uri: 'https://res.cloudinary.com/lirvin/image/upload/v1553715269/' + this.state.photo + 3 + '.heic' }}
+              style={{ width: 325, height: 415}}
+              blurRadius={this.state.hintTwoBlur}/>
+          </View>
+        </Swiper>
         <TouchableOpacity
             style={styles.button}>
           <Text style={styles.found}> Found it! </Text>
