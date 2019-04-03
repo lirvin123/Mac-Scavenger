c3ae5ead275688dff8ae6623bd683aaeef0d85b7
malqatam <malqatam@macalester.edu>
2019-04-03 14:01:55 -0500

    Added stopwatch (unfortunately timer too)

--- Screens/Main.js
+++ Screens/Main.js
@@ -1,23 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image, Platform, Button, ScrollView, Alert } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TouchableHighlight, TouchableOpacity, Image, Platform, Button, ScrollView, Alert } from 'react-native';
 import AppNavigator from '../navigator/appNavigator';
 import Swiper from 'react-native-swiper';
+import {Stopwatch, Timer} from 'react-native-stopwatch-timer'
 import {BlurView} from 'expo';
 import Styles from '../assets/styles';
 import Photos from '../photos.json';
 
 export default class Main extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.toggleTimer = this.toggleTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);
+    this.toggleStopwatch = this.toggleStopwatch.bind(this);
+    this.resetStopwatch = this.resetStopwatch.bind(this);
     this.state = {
       photoPath: Photos.pathName,
       hintOneBlur: 100,
       hintTwoBlur: 100,
       hintOneUnlocked: false,
-      hintTwoUnlocked: false
+      hintTwoUnlocked: false,
+      timerStart: false,
+      stopwatchStart: false,
+      totalDuration: 90000,
+      timerReset: false,
+      stopwatchReset: false,
     }
   }
+  toggleTimer() {
+    this.setState({timerStart: !this.state.timerStart, timerReset: false});
+  }
+
+  resetTimer() {
+    this.setState({timerStart: false, timerReset: true});
+  }
+
+  toggleStopwatch() {
+    this.setState({stopwatchStart: !this.state.stopwatchStart, stopwatchReset: false});
+  }
+
+  resetStopwatch() {
+    this.setState({stopwatchStart: false, stopwatchReset: true});
+  }
+
+  getFormattedTime(time) {
+      this.currentTime = time;
+  };
 
   onSwipe(index) {
     if (index == 1 || index == 2) {
@@ -85,7 +114,30 @@ export default class Main extends React.Component {
             onPress={() => this.props.navigation.navigate('Riddle')}>
           <Text style={Styles.buttonText}> Found it! </Text>
         </TouchableOpacity>
+
+
+        <Stopwatch laps msecs start={this.state.stopwatchStart}
+          reset={this.state.stopwatchReset}
+          getTime={this.getFormattedTime} />
+        <TouchableHighlight onPress={this.toggleStopwatch}>
+          <Text style={{fontSize: 30}}>{!this.state.stopwatchStart ? "Start" : "Stop"}</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.resetStopwatch}>
+          <Text style={{fontSize: 30}}>Reset</Text>
+        </TouchableHighlight>
+        <Timer totalDuration={this.state.totalDuration} msecs start={this.state.timerStart}
+          reset={this.state.timerReset}
+          handleFinish={handleTimerComplete}
+          getTime={this.getFormattedTime} />
+        <TouchableHighlight onPress={this.toggleTimer}>
+          <Text style={{fontSize: 30}}>{!this.state.timerStart ? "Start" : "Stop"}</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this.resetTimer}>
+          <Text style={{fontSize: 30}}>Reset</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
+
+const handleTimerComplete = () => alert("custom completion function");
