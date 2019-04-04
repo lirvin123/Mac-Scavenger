1d18f03a2f1413e72c90d602a75cbf37c72e34ee
Lily <lily.irvin@gmail.com>
2019-04-03 21:51:10 -0500

    Dynamic round labels actually working

--- Screens/Main.js
+++ Screens/Main.js
@@ -102,7 +102,7 @@ export default class Main extends React.Component {
     return (
       <View
           style={Styles.container}>
-        <Text style={Styles.round}> {"Round " + photoIndex + 1} </Text>
+        <Text style={Styles.round}> {"Round " + (photoIndex + 1)} </Text>
         <Swiper
             loop={false}
             onIndexChanged={(index) => this.onSwipe(index)}
