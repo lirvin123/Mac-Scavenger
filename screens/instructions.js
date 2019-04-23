f82433640df3a1c1f35aa17e740aa189b95ae1e1
Lily <lily.irvin@gmail.com>
2019-04-23 09:49:51 -0500

    Fixed instruction page bug

--- screens/instructions.js
+++ screens/instructions.js
@@ -13,13 +13,13 @@ export default class Instructions extends React.Component {
 
     return (
     <View style={Styles.hunt}>
-      <Text style={Styles.done}> {"Welcome to " + '\"' + hunt.huntName + '\"' }  </Text>
-      // <Text style={Styles.done}> {"Rounds:" hints.length }  </Text>
-      // <Text style={Styles.done}> {hunt.description}  </Text>
-      // <Text style={Styles.done}> {"Geographic Range:"" + hunt.geographicrange}  </Text>
-      // <Text style={Styles.done}> {"Unlocking the second photo will add 5 minutes to your timer,
-      //                             and unlocking the third photo will add 10 minutes to your timer
-      //                             Giving up will add twenty minutes to your timer.""}  </Text>
+      <Text style={Styles.riddle}> {"Welcome to " + '\"' + hunt.huntName + '\"' } </Text>
+      <Text style={{fontSize: 15, textAlign: "center"}}> {"Rounds:" + hunt.hints.length} </Text>
+      <Text style={{fontSize: 15, textAlign: "center"}}> {hunt.description}  </Text>
+      <Text style={{fontSize: 15, textAlign: "center"}}> {"Geographic Range:" + hunt.geographicrange}  </Text>
+      <Text style={{fontSize: 15, textAlign: "center"}}>  Unlocking the second photo will add 5 minutes to your timer,
+                                                          and unlocking the third photo will add 10 minutes to your timer
+                                                          Giving up will add twenty minutes to your timer.</Text>
       <Button danger block large onPress={() => this.props.navigation.navigate('Main')} style={Styles.button}>
         <Text style={Styles.buttonText}> Start: timer will start immediately </Text>
       </Button>
